95dcb7422497483f2342c990eeb533a659398930
bigbosstony <macintosh711@gmail.com>
2016-10-13 12:10:38 -0400

    add payment screen

---
+++ smartparkinglot/src/main/java/humber/thunderbolts/PaymentScreen.java
@@ -0,0 +1,13 @@
+package humber.thunderbolts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_screen);
+    }
+}
