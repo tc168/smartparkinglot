d44b0836d465c899f40f49dbbda548f762e5d3e0
tc168 <thomas.chang@hotmail.ca>
2016-11-23 17:47:29 -0500

    Fix the merge of Yang Yu

---
+++ smartparkinglot/src/main/java/humber/thunderbolts/WalletActivity.java
@@ -0,0 +1,16 @@
+package humber.thunderbolts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yan Yu (n00769714) on 2016-11-21.
+ */
+
+public class WalletActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+    }
+}
