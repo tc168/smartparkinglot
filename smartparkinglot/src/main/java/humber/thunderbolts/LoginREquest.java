4dfd661926f2b04007e5adcd15aae12d0e48fde1
Achille <achille.mattapullut@gmail.com>
2016-12-09 17:27:40 -0500

    login part complete

---
+++ smartparkinglot/src/main/java/humber/thunderbolts/LoginREquest.java
@@ -0,0 +1,28 @@
+package humber.thunderbolts;
+
+/**
+ * Created by Achille on 2016-12-09.
+ */
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+    private static final String LOGIN_REQUEST_URL = "https://achille.000webhostapp.com/Login.php";
+    private Map<String, String> params;
+
+    public LoginRequest(String username, String password, Response.Listener<String> listener) {
+        super(Method.POST, LOGIN_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
