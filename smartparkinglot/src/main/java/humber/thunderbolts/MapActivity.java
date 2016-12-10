79745a2d5211b16e80d0a99dd81e5bac256cdf5b
tc168 <thomas.chang@hotmail.ca>
2016-12-10 18:45:35 -0500

    added permisison and using mylocation if user wants . When there is no permisison it will set to default parking lot of humber

--- smartparkinglot/src/main/java/humber/thunderbolts/MapActivity.java
+++ smartparkinglot/src/main/java/humber/thunderbolts/MapActivity.java
@@ -1,24 +1,34 @@
 //Thomas Chang
 package humber.thunderbolts;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -32,13 +42,16 @@ import humber.thunderbolts.parking.ParkingSpot;
 
 //import humber.thunderbolts.parking.ConnectDatabase;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
-
     private ConnectDatabase con;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
 
-
+    private static final int LOCATION_PERMISSION = 100;
 
 
     @Override
@@ -52,16 +65,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         con = new ConnectDatabase();
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -72,9 +75,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+
+
+
+
+       MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
+
+
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -87,50 +97,87 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-
-        ArrayList<ParkingSpot> listOfParkingSpots = null;
-        try {
-            listOfParkingSpots = con.execute().get();
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        assert listOfParkingSpots != null;
-        for (ParkingSpot p : listOfParkingSpots){
-            LatLng parkSpot = new LatLng(p.getLongitude(),p.getLatitude());
-            float color = (p.isSpotTaken()) ? BitmapDescriptorFactory.HUE_BLUE : BitmapDescriptorFactory.HUE_RED;
-            MarkerOptions markerOption = new MarkerOptions()
-                    .position(parkSpot).title(p.getLicensePlate())
-                    .icon(BitmapDescriptorFactory.defaultMarker(color));
-
-            Marker marker =  mMap.addMarker(markerOption);
-            marker.showInfoWindow();
-
-        }
+       mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
 
 
-        LatLng humber = new LatLng(43.72952382003048, -79.60450954735279);
-        // mMap.addMarker(new MarkerOptions().position(humber).title("Marker in Humber Parking"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(humber));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(18.5f));
 
 
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        double lat;
+        double lng;
+        float zoom;
 
-            @Override
-            public void onMapClick(LatLng arg0) {
-
-                Log.d("arg0", arg0.latitude + "-" + arg0.longitude);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                //Location Permission already granted
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                mMap.getUiSettings().setCompassEnabled(true);
+            } else {
+                //Request Location Permission
+                getLocationPermission();
             }
-        });
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
 
+
+        getParkingList(googleMap);
+
+        lat = Double.parseDouble(getString(R.string.defaultLat));
+        lng = Double.parseDouble(getString(R.string.defaultLong));
+        zoom = Float.parseFloat(getString(R.string.defaultZoom));
+
+
+        LatLng setLocation = new LatLng(lat, lng);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(setLocation));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(zoom));
+
+
+
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
     }
 
 
@@ -159,7 +206,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-      // getMenuInflater().inflate(R.menu.main, menu);
+        // getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -168,7 +215,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-      //  int id = item.getItemId();
+        //  int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
 
@@ -212,7 +259,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
 
 
-            //deleted old payment
+        //deleted old payment
         else if (id == R.id.drawer_login) {
 
             Intent intentLoginActivity = new Intent(this, LoginActivity.class);
@@ -228,4 +275,129 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
+    private ArrayList<ParkingSpot> getParkingList(GoogleMap googleMap) {
+
+        ArrayList<ParkingSpot> listOfParkingSpots = null;
+        try {
+            listOfParkingSpots = con.execute().get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+        for (ParkingSpot p : listOfParkingSpots) {
+            LatLng parkSpot = new LatLng(p.getLongitude(), p.getLatitude());
+            float color = (p.isSpotTaken()) ? BitmapDescriptorFactory.HUE_BLUE : BitmapDescriptorFactory.HUE_RED;
+            MarkerOptions markerOption = new MarkerOptions()
+                    .position(parkSpot).title(p.getLicensePlate())
+                    .icon(BitmapDescriptorFactory.defaultMarker(color));
+
+            Marker marker = googleMap.addMarker(markerOption);
+            marker.showInfoWindow();
+
+        }
+
+        return listOfParkingSpots;
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+    private boolean checkLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        //System.out.print(map);
+        if (checkLocationPermission() && mMap != null)
+
+            mMap.setMyLocationEnabled(true);
+
+
+        System.out.println("location " + location);
+
+
+        double latitude = location.getLatitude();
+
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        System.out.println("Lat " + latitude);
+        System.out.println("long " + longitude);
+
+        LatLng myPosition = new LatLng(latitude, longitude);
+
+        // Add Makrer
+
+
+        // Zoom into my location
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+
+    private void getLocationPermission() {
+        boolean returnCheck = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+
+
+        if (returnCheck) {
+
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_PERMISSION);
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+
+                    }
+
+                }
+
+                // other 'case' lines to check for other
+                // permissions this app might request
+            }
+        }
+    }
+
+
 }
