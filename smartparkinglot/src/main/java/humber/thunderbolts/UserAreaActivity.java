70b7258f5cd5d871903c4c1fbb71c10444b71c25
Achille <achille.mattapullut@gmail.com>
2016-12-11 17:18:43 -0500

    login part complete + tweaks

--- smartparkinglot/src/main/java/humber/thunderbolts/UserAreaActivity.java
+++ smartparkinglot/src/main/java/humber/thunderbolts/UserAreaActivity.java
@@ -1,12 +1,19 @@
 package humber.thunderbolts;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+
 public class UserAreaActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,7 @@ public class UserAreaActivity extends AppCompatActivity {
         String username = intent.getStringExtra("username");
         int age = intent.getIntExtra("age", -1);
 
+
         TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
         EditText etUsername = (EditText) findViewById(R.id.etUsername);
         EditText etAge = (EditText) findViewById(R.id.etAge);
@@ -25,6 +33,7 @@ public class UserAreaActivity extends AppCompatActivity {
         String message = name + " welcome to your user area";
         tvWelcomeMsg.setText(message);
         etUsername.setText(username);
-        etAge.setText(age + "");
+
+
     }
 }
