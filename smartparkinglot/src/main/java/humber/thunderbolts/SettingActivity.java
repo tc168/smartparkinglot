a74ffca3db6c4bd17b60b02cb7e84a9955cc0a38
singhsaab <n01075212>
2016-12-11 16:03:40 -0500

    themes

--- smartparkinglot/src/main/java/humber/thunderbolts/SettingActivity.java
+++ smartparkinglot/src/main/java/humber/thunderbolts/SettingActivity.java
@@ -8,24 +8,26 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
-import static humber.thunderbolts.R.styleable.CompoundButton;
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener
+{
 
-public class SettingActivity extends AppCompatActivity {
 
-    static int flag=1;
     private TextView switchStatus;
     private Switch mySwitch;
-    private TextView switchStatus2;
-    private Switch mySwitch2;
+    ToggleButton btn;
+    static int flag=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Theme.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_setting);
         switchStatus = (TextView) findViewById(R.id.switchStatus);
         mySwitch = (Switch) findViewById(R.id.mySwitch);
-        switchStatus2 = (TextView) findViewById(R.id.switchStatus2);
-        mySwitch2 = (Switch) findViewById(R.id.mySwitch2);
+        btn=(ToggleButton)findViewById(R.id.toggleButton);
+        findViewById(R.id.toggleButton).setOnClickListener(this);
+
+
         //set the switch to ON
         mySwitch.setChecked(true);
         //attach a listener to check for changes in state
@@ -35,9 +37,9 @@ public class SettingActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
 
-                if (isChecked) {
+                if(isChecked){
                     switchStatus.setText("Switch is currently ON");
-                } else {
+                }else{
                     switchStatus.setText("Switch is currently OFF");
                 }
 
@@ -45,50 +47,38 @@ public class SettingActivity extends AppCompatActivity {
         });
 
         //check the current state before we display the screen
-        if (mySwitch.isChecked()) {
+        if(mySwitch.isChecked()){
             switchStatus.setText("Switch is currently ON");
-        } else {
+        }
+        else {
             switchStatus.setText("Switch is currently OFF");
         }
     }
-
-   /*// mySwitch2.setChecked(true);
-    //attach a listener to check for changes in state
-   // mySwitch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-
+    @Override
+    public void onClick(View v)
     {
+        if (((ToggleButton) v).isChecked())
+        {
+            flag=flag+1;
+        }
 
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView,
-        boolean isChecked) {
 
-        if (isChecked) {
-            switchStatus2.setText("Switch is currently ON");
-        } else {
-            switchStatus2.setText("Switch is currently OFF");
+        if(flag%2==0)
+        {
+            Theme.changeToTheme(this,Theme.BlueTheme);
+            setContentView(R.layout.activity_setting);
+
+
+        }
+        else
+        {
+            Theme.changeToTheme(this,Theme.GreenTheme);
+            setContentView(R.layout.activity_setting);
+
+
         }
 
     }
-    });
-
-    //check the current state before we display the screen
-    if(mySwitch2.isChecked())
-    {
-        flag=flag+1;
-    }
-
-
-    if(flag%2==0)
-    {
-        Theme.changeToTheme(this,Theme.BlueTheme);
-        setContentView(R.layout.activity_setting);
-    }
-    else
-    {
-        Theme.changeToTheme(this,Theme.GreenTheme);
-        setContentView(R.layout.activity_setting);
-    }
-*/
 }
 
 
